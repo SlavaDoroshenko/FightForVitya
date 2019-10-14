d5bd957a08fdf48117d60eab5ae8f87128ea162a
SlavaDoroshenko <lblue1256@gmail.com>
2019-10-14 12:12:34 +0400

    Far

---
+++ src/Dog.java
@@ -0,0 +1,35 @@
+public class Dog {
+    private int age;
+    private String color;
+    private String name;
+
+    Dog(int age, String color, String name) {
+        this.age = age;
+        this.color = color;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
